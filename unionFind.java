0ad697c22bbb53b97685da252fa5def8a42be940
Prudhvi K <88816987+prudhvinag06@users.noreply.github.com>
2022-02-10 22:33:22 +0530

    Update unionFind.java

--- unionFind.java
+++ unionFind.java
@@ -1,4 +1,33 @@
-//Call using the class object
+//Leetcode 200
+
+class Solution {
+    public int numIslands(char[][] grid){
+        UnionFind uf = new UnionFind(grid);
+        int m = grid.length; 
+        int n = grid[0].length;
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    for(int k = 0; k < 4; k++){
+                        int new_x = dx[k] + i;
+                        int new_y = dy[k] + j;
+                        if(new_x < 0 || new_x >= m || new_y < 0 || new_y >= n || grid[new_x][new_y] == '0')
+                            continue;
+                        int id1 = i * n + j;
+                        int id2 = new_x * n + new_y;
+                        
+                        uf.union(id1, id2);
+                        
+                    }
+                }
+            }
+        }
+        
+        return uf.count;
+    }
+}
 
 class UnionFind{
     int count = 0;
