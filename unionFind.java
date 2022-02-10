a227aad2ca168892ebb1ccb54ac83dc9a71e0060
Prudhvi K <88816987+prudhvinag06@users.noreply.github.com>
2022-02-10 22:32:19 +0530

    Update unionFind.java

--- unionFind.java
+++ unionFind.java
@@ -1,1 +1,40 @@
+//Call using the class object
 
+class UnionFind{
+    int count = 0;
+    int parent[];
+    int m, n; 
+    
+    UnionFind(char[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+        parent = new int[m * n];
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    int id = i * n + j;
+                    parent[id] = id;
+                    count++;
+                }
+            }
+        }
+    }
+    
+    public void union(int u, int v){
+        int parent_u = findParent(u);
+        int parent_v = findParent(v);
+        
+        if(parent_u != parent_v){
+            parent[parent_u] = parent_v;
+            count--;
+        }
+    }
+    
+    public int findParent(int u){
+        if(u == parent[u]){
+            return u;
+        }
+        return parent[u] = findParent(parent[u]);
+    }
+}
